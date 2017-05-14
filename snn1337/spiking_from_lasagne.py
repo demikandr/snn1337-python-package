9d4066ab5c696f5d422b9610de2d427724bd63b7
annnvl <no_love_no_problem@MacBook-Air-no-love-no-problem.local>
2017-05-14 14:46:16 +0300

    history + some fixes of the network

--- snn1337/spiking_from_lasagne.py
+++ snn1337/spiking_from_lasagne.py
@@ -2,10 +2,12 @@
 from snn1337.layers import *
 import lasagne
 
-def spiking_from_lasagne(input_net, threshold):
+def spiking_from_lasagne(input_net, threshold=1.):
     input_layers = lasagne.layers.get_all_layers(input_net)
     weights = lasagne.layers.get_all_param_values(input_net)
     spiking_net = NNet(input_layers[0].shape[-3:], threshold)
+    if(len(threshold) == 1):
+        threshold = np.ones(len(input_layers)-1) * threshold
     convert_layers = {lasagne.layers.conv.Conv2DLayer : spiking_net.add_convolution,\
                       lasagne.layers.dense.DenseLayer : spiking_net.add_dense}
     
@@ -16,7 +18,7 @@ def spiking_from_lasagne(input_net, threshold):
         if(type(l) == lasagne.layers.pool.Pool2DLayer or type(l) == lasagne.layers.pool.MaxPool2DLayer):
             spiking_net.add_subsampling(l.pool_size)
         else:
-            convert_layers[type(l)](weights[i])
+            convert_layers[type(l)](weights[2*i], threshold=threshold[i-1])
             i+=1
-
+            
     return spiking_net
